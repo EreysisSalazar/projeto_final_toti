903d4cd9a0079310b18a8348aba09a147ab0e8a8
ragevoid <riccardo.escorche@gmail.com>
2023-09-09 20:22:02 -0300

    Fazendo front-end e testes varios

--- script.js
+++ script.js
@@ -402,6 +402,7 @@ let teamData = [
       ]
   }
 ];
+
 let newPlayer = {
     id: 12345,
     name: "Pedrito Perez",
@@ -447,12 +448,20 @@ function showSquad() {
       playerCardDiv.classList.add('playerCardDiv');
       playerCardDiv.setAttribute("id", `${player.id}`);
 
+      let playerImagesDiv = document.createElement('div')
+
       let playerPhoto = document.createElement('img');
       playerPhoto.src = player.photo;
       playerPhoto.alt = player.name;
 
+      let playerTeam = document.createElement('img')
+      playerTeam.src = 'img/logoFlamengo.png'
+      playerTeam.alt='Flamengo'
+
       let playerName = document.createElement('p');
-      playerName.textContent = `Name: ${player.name}`;
+      playerName.textContent = `${player.name}`;
+      playerName.classList.add('playerName');
+
 
       let playerAge = document.createElement('p');
       playerAge.textContent = `Age: ${player.age}`;
@@ -473,14 +482,16 @@ function showSquad() {
       playerDeleteButton.classList.add('playerButton');
       playerDeleteButton.textContent = `Apagar Dados`;
 
-
-      playerCardDiv.appendChild(playerPhoto);
       playerCardDiv.appendChild(playerName);
+      playerCardDiv.appendChild(playerImagesDiv);
+    //playerImagesDivappendChild(playerTeam);
+    playerImagesDiv.appendChild(playerPhoto);
       playerCardDiv.appendChild(playerAge);
       playerCardDiv.appendChild(playerNumber);
       playerCardDiv.appendChild(playerPosition);
       playerCardDiv.appendChild(playerUpdateButton);
       playerCardDiv.appendChild(playerDeleteButton);
+
 
       teamListDiv.appendChild(playerCardDiv);
     });
@@ -492,90 +503,90 @@ showSquad();
 
 
 
-// FUNÇAO que faz o POST para criar un novo jogador 
-function sendPlayer(newPlayer) {
-  fetch('https://v3.football.api-sports.io/players', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'x-rapidapi-host': 'v3.football.api-sports.io',
-      'x-rapidapi-key': '83303917d80ac4e0dad6d309eabe233e'
-    },
-    body: JSON.stringify(newPlayer)
-  })
-    .then(response => response.json())
-    .then(data => {
-        if (newPlayer.id === 0 || teamData.some(player => player.id === newPlayer.id)) {
-        let newID;
-        do {
-          newID = Math.floor(Math.random() * 10000);
-        } while (teamData.some(player => player.id === nuevoID));
-        newPlayer.id = newID;
-      } 
-        teamData.push(newPlayer);
-        return teamData;
-    });
-    console.log("O Novo jogador do time é:",teamData.find(player => player.id === newPlayer.id))
-}
+// // FUNÇAO que faz o POST para criar un novo jogador 
+// function sendPlayer(newPlayer) {
+//   fetch('https://v3.football.api-sports.io/players', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'x-rapidapi-host': 'v3.football.api-sports.io',
+//       'x-rapidapi-key': '83303917d80ac4e0dad6d309eabe233e'
+//     },
+//     body: JSON.stringify(newPlayer)
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//         if (newPlayer.id === 0 || teamData.some(player => player.id === newPlayer.id)) {
+//         let newID;
+//         do {
+//           newID = Math.floor(Math.random() * 10000);
+//         } while (teamData.some(player => player.id === nuevoID));
+//         newPlayer.id = newID;
+//       } 
+//         teamData.push(newPlayer);
+//         return teamData;
+//     });
+//     console.log("O Novo jogador do time é:",teamData.find(player => player.id === newPlayer.id))
+// }
 
 
-// FUNÇAO que modifica com o metodo PUT a um jogador
-function updatePlayer(playerID, newData) {
-fetch(`https://v3.football.api-sports.io/players/?id=${playerId}`, {
-method: 'PUT',
-headers: {
-  'Content-Type': 'application/json',
-  'x-rapidapi-host': 'v3.football.api-sports.io',
-  'x-rapidapi-key': '83303917d80ac4e0dad6d309eabe233e'
-},
-body: JSON.stringify(newData)
-})
-.then(response => response.json())
-.then(data => {  
-    let playerIndex = teamData.findIndex(player => player.id === playerID);
-    if (playerIndex !== -1) {
-      teamData[playerIndex] = {
-        ...teamData[playerIndex],
-        ...newData
-      };
-      console.log("O Jogador alterado foi:",teamData.find(player => player.id === PlayerID))   
-      return teamData;
-    } else {
-      console.log("ID inexistente");
-      return teamData;
-    }
-})
-}
+// // FUNÇAO que modifica com o metodo PUT a um jogador
+// function updatePlayer(playerID, newData) {
+// fetch(`https://v3.football.api-sports.io/players/?id=${playerId}`, {
+// method: 'PUT',
+// headers: {
+//   'Content-Type': 'application/json',
+//   'x-rapidapi-host': 'v3.football.api-sports.io',
+//   'x-rapidapi-key': '83303917d80ac4e0dad6d309eabe233e'
+// },
+// body: JSON.stringify(newData)
+// })
+// .then(response => response.json())
+// .then(data => {  
+//     let playerIndex = teamData.findIndex(player => player.id === playerID);
+//     if (playerIndex !== -1) {
+//       teamData[playerIndex] = {
+//         ...teamData[playerIndex],
+//         ...newData
+//       };
+//       console.log("O Jogador alterado foi:",teamData.find(player => player.id === PlayerID))   
+//       return teamData;
+//     } else {
+//       console.log("ID inexistente");
+//       return teamData;
+//     }
+// })
+// }
 
 
-// FUNÇAO que apaga o jogador usando DELETE
-function deletePlayer(playerID) {
-fetch(`https://v3.football.api-sports.io/players/?id=${playerID}`, {
-method: 'DELETE',
-headers: {
-  'x-rapidapi-host': 'v3.football.api-sports.io',
-  'x-rapidapi-key': '83303917d80ac4e0dad6d309eabe233e'
-}
-})
-.then(response => response.json())
-.then(data => {
-    let playerIndex = teamData.findIndex(player => player.id === playerID);
-    if (playerIndex !== -1) {
-      teamData.splice(playerIndex, 1);
-      console.log("Jogador apagado com sucesso.");
-    } else {
-      console.log("ID inexistente.");
-    }
-    return teamData
-  }
-)}
+// // FUNÇAO que apaga o jogador usando DELETE
+// function deletePlayer(playerID) {
+// fetch(`https://v3.football.api-sports.io/players/?id=${playerID}`, {
+// method: 'DELETE',
+// headers: {
+//   'x-rapidapi-host': 'v3.football.api-sports.io',
+//   'x-rapidapi-key': '83303917d80ac4e0dad6d309eabe233e'
+// }
+// })
+// .then(response => response.json())
+// .then(data => {
+//     let playerIndex = teamData.findIndex(player => player.id === playerID);
+//     if (playerIndex !== -1) {
+//       teamData.splice(playerIndex, 1);
+//       console.log("Jogador apagado com sucesso.");
+//     } else {
+//       console.log("ID inexistente.");
+//     }
+//     return teamData
+//   }
+// )}
 
-// Eventos de escuta que salva os dados de jogador novo
-document.getElementById('submitBtn').addEventListener('click', function () {
-  let playerName = document.getElementById('name').value;
-  let playerAge = document.getElementById('age').value;
-  let playerNumber = document.getElementById('number').value;
-  let playerPosition = document.getElementById('position').value;
-  let playerPhoto = document.getElementById('photo').value;
-  console.log(playerData);
-});
+// // Eventos de escuta que salva os dados de jogador novo
+// document.getElementById('submitBtn').addEventListener('click', function () {
+//   let playerName = document.getElementById('name').value;
+//   let playerAge = document.getElementById('age').value;
+//   let playerNumber = document.getElementById('number').value;
+//   let playerPosition = document.getElementById('position').value;
+//   let playerPhoto = document.getElementById('photo').value;
+//   console.log(playerData);
+// });
